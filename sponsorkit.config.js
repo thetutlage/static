2ca8a6f3f48c544073a628da54f1d597c85a292a
Harminder Virk <virk.officials@gmail.com>
2022-07-08 16:15:45 +0530

    feat: add sponsors

---
+++ sponsorkit.config.js
@@ -0,0 +1,34 @@
+import { defineConfig, presets } from 'sponsorkit'
+import sponsorsList from './platinum_sponsors.json'
+
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Backers',
+      preset: presets.small,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 19,
+      preset: presets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 29,
+      preset: presets.medium,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.large,
+    },
+    {
+      title: 'Platinum Sponsors',
+      monthlyDollars: Infinity,
+      preset: presets.xl,
+      composeAfter(compose) {
+        compose.addTitle('Platinum Sponsors').addSponsorGrid(sponsorsList, presets.xl)
+      },
+    },
+  ],
+})
